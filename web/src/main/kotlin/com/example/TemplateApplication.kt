096963960bb3241e0d3e7b53c995fdfc68adcfd8
Gábor Bagita <bagitagab@gmail.com>
2021-04-18 19:07:04 +0200

    Creating separate JPA configuration class.

--- web/src/main/kotlin/com/example/TemplateApplication.kt
+++ web/src/main/kotlin/com/example/TemplateApplication.kt
@@ -1,13 +1,9 @@
 package com.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
-@EnableJpaAuditing
 class TemplateApplication
 
 fun main(args: Array<String>) {
