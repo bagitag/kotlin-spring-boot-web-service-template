11ac9df95dbbf0391b4fef52b145c434fe90b099
Gábor Bagita <bagitagab@gmail.com>
2021-04-24 12:03:05 +0200

    Solving the multi module property files problem.

---
+++ web/src/main/kotlin/com/example/configuration/ApplicationPropertiesConfiguration.kt
@@ -0,0 +1,15 @@
+package com.example.configuration
+
+import org.springframework.context.annotation.Profile
+import org.springframework.context.annotation.PropertySource
+import org.springframework.stereotype.Component
+
+@Component
+@Profile("dev")
+@PropertySource(value = [ "classpath:persistence-dev.properties" ])
+class DevProfile
+
+@Component
+@Profile("test")
+@PropertySource(value = [ "classpath:persistence-test.properties" ])
+class TestProfile
