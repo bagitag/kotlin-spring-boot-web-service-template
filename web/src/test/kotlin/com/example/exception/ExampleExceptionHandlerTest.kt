c86f0b072699736e8f299d63831ca2f7eefdf1b9
Gábor Bagita <bagitagab@gmail.com>
2021-04-26 10:21:18 +0200

    Turning on Spring Boot Actuator.

--- web/src/test/kotlin/com/example/exception/ExampleExceptionHandlerTest.kt
+++ web/src/test/kotlin/com/example/exception/ExampleExceptionHandlerTest.kt
@@ -1,9 +1,11 @@
 package com.example.exception
 
 import com.example.entity.Example
+import com.example.metrics.ExceptionMetrics
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -19,13 +21,16 @@ internal class ExampleExceptionHandlerTest {
 
     @MockK
     private lateinit var webRequest: WebRequest
+    @MockK
+    private lateinit var exceptionMetrics: ExceptionMetrics
 
     private lateinit var victim: ExampleExceptionHandler
 
     @BeforeEach
     fun initialize() {
-        victim = ExampleExceptionHandler(true)
+        victim = ExampleExceptionHandler(true, exceptionMetrics)
         every { webRequest.getParameterValues("trace") } returns Array(1) { "false" }
+        every { exceptionMetrics.updateExceptionCounter(any()) } returns Unit
     }
 
     @Test
@@ -36,7 +41,7 @@ internal class ExampleExceptionHandlerTest {
         val exceptionIdString = "com.example.exception.IdNotFoundException" +
                 "com.example.exception.ExampleExceptionHandlerTest" +
                 "Should generate response entity from known exception" +
-                35
+                40
         val expected = DigestUtils.md5DigestAsHex(exceptionIdString.toByteArray()).take(10)
 
         // when
@@ -57,7 +62,7 @@ internal class ExampleExceptionHandlerTest {
         val exceptionIdString = "com.example.exception.IdNotFoundException" +
                 "com.example.exception.ExampleExceptionHandlerTest" +
                 "Should generate response entity from known exception with stack trace" +
-                56
+                61
         val expected = DigestUtils.md5DigestAsHex(exceptionIdString.toByteArray()).take(10)
 
         // and
@@ -80,7 +85,7 @@ internal class ExampleExceptionHandlerTest {
         val exceptionIdString = "java.lang.RuntimeException" +
                 "com.example.exception.ExampleExceptionHandlerTest" +
                 "Should generate response entity from exception" +
-                79
+                84
         val expected = DigestUtils.md5DigestAsHex(exceptionIdString.toByteArray()).take(10)
 
         // when
@@ -100,7 +105,7 @@ internal class ExampleExceptionHandlerTest {
         val exceptionIdString = "java.lang.RuntimeException" +
                 "com.example.exception.ExampleExceptionHandlerTest" +
                 "Should generate response entity from exception with stack trace" +
-                99
+                104
         val expected = DigestUtils.md5DigestAsHex(exceptionIdString.toByteArray()).take(10)
 
         // and
@@ -114,5 +119,30 @@ internal class ExampleExceptionHandlerTest {
         assertEquals(ExampleExceptionHandler.unknownError, actual.body.message)
         assertEquals(expected, actual.body.id)
         assertFalse(actual.body.stackTrace.isNullOrEmpty())
+    }
+
+    @Test
+    fun `Should generate exception id and update the related counter`() {
+        // given
+        val id = 10L
+        val exception = IdNotFoundException(Example::class, id)
+        val exceptionIdString = "com.example.exception.IdNotFoundException" +
+                "com.example.exception.ExampleExceptionHandlerTest" +
+                "Should generate exception id and update the related counter" +
+                128
+        val expected = DigestUtils.md5DigestAsHex(exceptionIdString.toByteArray()).take(10)
+
+        // and
+        every { exceptionMetrics.updateExceptionCounter(expected) } returns Unit
+
+        // when
+        val actual = victim.handleIdNotFoundException(exception, webRequest)
+
+        // then
+        assertEquals(HttpStatus.NOT_FOUND, actual.statusCode)
+        assertEquals("Could not find Example with id: $id", actual.body.message)
+        assertEquals(expected, actual.body.id)
+        assertTrue(actual.body.stackTrace.isNullOrEmpty())
+        verify { exceptionMetrics.updateExceptionCounter(expected) }
     }
 }
