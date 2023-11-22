e18bc037db7b217cdd8dd059c6d5c29cc2876c01
Gábor Bagita <bagitagab@gmail.com>
2023-11-22 19:23:09 +0100

    Configuring Testcontainers for integration testing.

--- web/src/test/kotlin/com/example/BaseIntegrationTest.kt
+++ web/src/test/kotlin/com/example/BaseIntegrationTest.kt
@@ -1,9 +1,27 @@
 package com.example
 
+import org.junit.jupiter.api.BeforeAll
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Testcontainers
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-    properties = [ "spring.profiles.active=test" ])
+@Testcontainers
 @ActiveProfiles("test")
-class BaseIntegrationTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+abstract class BaseIntegrationTest {
+
+    companion object {
+        @ServiceConnection
+        private val databaseContainer = PostgreSQLContainer<Nothing>("postgres:latest")
+
+        @JvmStatic
+        @BeforeAll
+        fun beforeAll() {
+            databaseContainer.start()
+        }
+    }
+}
