76d6c84d7a77a7a6ef8dc712d3aba535cb2b1e61
Gábor Bagita <bagitagab@gmail.com>
2023-12-21 16:42:13 +0100

    Implementing debug level logging for specific requests.

---
+++ web/src/main/kotlin/com/example/configuration/DebugLoggingTurboFilter.kt
@@ -0,0 +1,41 @@
+package com.example.configuration
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.turbo.MDCFilter
+import ch.qos.logback.core.spi.FilterReply
+import com.example.TemplateApplication
+import org.slf4j.Marker
+
+class DebugLoggingTurboFilter : MDCFilter() {
+
+    private lateinit var packages: String
+
+    fun setPackages(packages: String) {
+        this.packages = packages
+    }
+
+    override fun decide(
+        marker: Marker?, logger: Logger?, level: Level?, format: String?, params: Array<out Any>?, t: Throwable?
+    ): FilterReply {
+        if (!isStarted) {
+            return FilterReply.NEUTRAL
+        }
+
+        var filterReply = super.decide(marker, logger, level, format, params, t)
+
+        if (filterReply === FilterReply.ACCEPT) {
+            filterReply = extendPackagesWithProjectBasePackage().split(",").stream()
+                .filter { e -> logger?.name?.startsWith(e) ?: false }
+                .map { FilterReply.ACCEPT }
+                .findAny()
+                .orElse(FilterReply.DENY)
+        }
+        return filterReply
+    }
+
+    private fun extendPackagesWithProjectBasePackage(): String {
+        val basePackage = TemplateApplication::class.java.`package`.name
+        return "$basePackage,$packages"
+    }
+}
