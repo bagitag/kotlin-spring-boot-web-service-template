a7876df49a2532015c57222331bbaf0250f07f3f
Gábor Bagita <bagitagab@gmail.com>
2025-03-02 22:11:59 +0100

    Creating logic for handling history entities.

---
+++ template-project-persistence/src/main/kotlin/com/example/templateproject/persistence/entity/mapper/ExampleHistoryMapper.kt
@@ -0,0 +1,18 @@
+package com.example.templateproject.persistence.entity.mapper
+
+import com.example.templateproject.persistence.entity.Example
+import com.example.templateproject.persistence.entity.history.ExampleHistory
+import com.example.templateproject.persistence.entity.history.HistoryEvent
+import org.springframework.stereotype.Service
+
+@Service
+class ExampleHistoryMapper : AbstractHistoryMapper<Example, ExampleHistory>() {
+
+    override fun toHistoryEntity(entity: Example, event: HistoryEvent): ExampleHistory {
+        return ExampleHistory(
+            entity.id!!,
+            event,
+            entity.name
+        )
+    }
+}
