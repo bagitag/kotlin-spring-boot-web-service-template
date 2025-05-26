c2ee18dd0b1726739dc6d51b5b8284d0fdef8c39
Gábor Bagita <bagitagab@gmail.com>
2025-05-26 17:36:32 +0200

    Adding custom spans.

--- template-project-core/src/main/kotlin/com/example/templateproject/core/mapper/ExampleMapper.kt
+++ template-project-core/src/main/kotlin/com/example/templateproject/core/mapper/ExampleMapper.kt
@@ -4,6 +4,8 @@ import com.example.templateproject.api.dto.ExampleDTO
 import com.example.templateproject.persistence.entity.Example
 import io.micrometer.core.annotation.Timed
 import com.example.templateproject.persistence.repository.ExampleRepository
+import io.micrometer.tracing.annotation.NewSpan
+import io.micrometer.tracing.annotation.SpanTag
 import org.springframework.stereotype.Service
 import java.util.Optional
 
@@ -12,7 +14,8 @@ class ExampleMapper(repository: ExampleRepository) : AbstractMapper<Example, Exa
 
     @Timed(value = "app.method.executions", extraTags = ["topic", "example-entity-mapping"],
         description = "The amount of time spent in the different methods.")
-    override fun toEntity(dto: ExampleDTO): Example {
+    @NewSpan("dto-to-entity-mapping")
+    override fun toEntity(@SpanTag("dto") dto: ExampleDTO): Example {
         return Optional.ofNullable(dto.id)
             .map { getEntityId(it) }
             .map { it.apply { name = dto.name } }
@@ -21,7 +24,8 @@ class ExampleMapper(repository: ExampleRepository) : AbstractMapper<Example, Exa
 
     @Timed(value = "app.method.executions", extraTags = ["topic", "example-entity-mapping"],
         description = "The amount of time spent in the different methods.")
-    override fun toDTO(entity: Example) = ExampleDTO(
+    @NewSpan("entity-to-dto-mapping")
+    override fun toDTO(@SpanTag("entity") entity: Example) = ExampleDTO(
         entity.name
     ).apply {
         setBaseDTOFields(this, entity)
