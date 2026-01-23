5fb3db177bf0b17c947d2f75da6e5cab4c3006b3
Gábor Bagita <bagitagab@gmail.com>
2026-01-23 23:59:57 +0100

    Minor refactoring

--- template-project-core/src/main/kotlin/com/example/templateproject/core/mapper/AbstractMapper.kt
+++ template-project-core/src/main/kotlin/com/example/templateproject/core/mapper/AbstractMapper.kt
@@ -3,21 +3,22 @@ package com.example.templateproject.core.mapper
 import com.example.templateproject.api.dto.BaseDTO
 import com.example.templateproject.core.exception.IdNotFoundException
 import com.example.templateproject.persistence.entity.BaseEntity
-import com.example.templateproject.persistence.entity.Example
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
+import kotlin.reflect.KClass
 
 abstract class AbstractMapper<E : BaseEntity, D : BaseDTO>(
-    open val repository: JpaRepository<E, Long>,
+    protected open val repository: JpaRepository<E, Long>,
+    protected open val clazz: KClass<E>,
 ) {
     abstract fun toEntity(dto: D): E
 
     abstract fun toDTO(entity: E): D
 
     protected fun getEntityId(id: Long): E =
-        repository.findById(id).orElseThrow { IdNotFoundException(Example::class, id) }
+        repository.findById(id).orElseThrow { IdNotFoundException(clazz, id) }
 
     protected fun setBaseDTOFields(
         dto: D,
